3e4e4f8a8e0b1f47da01f732a69bf8a37d34efec
Yosua Haloho <72293596+YosuaPWH@users.noreply.github.com>
2024-01-15 01:35:16 +0700

    refactor: add nullable to some response api

--- app/src/main/java/com/synrgy/wefly/data/api/login/LoginResponse.kt
+++ app/src/main/java/com/synrgy/wefly/data/api/login/LoginResponse.kt
@@ -4,16 +4,16 @@ import com.google.gson.annotations.SerializedName
 
 data class LoginResponse(
     @SerializedName("access_token")
-    val accessToken: String,
+    val accessToken: String?,
     @SerializedName("refresh_token")
-    val refreshToken: String,
-    val code: Int,
-    val scope: String,
+    val refreshToken: String?,
+    val code: Int?,
+    val scope: String?,
     @SerializedName("token_type")
-    val tokenType: String,
-    val message: String,
+    val tokenType: String?,
+    val message: String?,
     @SerializedName("expires_in")
-    val exp: Int,
-    val jti: String,
+    val exp: Int?,
+    val jti: String?,
     val error: String?
 )
