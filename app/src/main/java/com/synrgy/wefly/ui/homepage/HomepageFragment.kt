40385b6a353eeb905b3113c5c499d6d05675af53
laetuz <laetuzg@gmail.com>
2024-01-24 13:54:18 +0700

    - Implemented logic for spinner with airport list API

--- app/src/main/java/com/synrgy/wefly/ui/homepage/HomepageFragment.kt
+++ app/src/main/java/com/synrgy/wefly/ui/homepage/HomepageFragment.kt
@@ -3,7 +3,9 @@ package com.synrgy.wefly.ui.homepage
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -11,7 +13,6 @@ import androidx.navigation.fragment.findNavController
 import com.synrgy.wefly.R
 import com.synrgy.wefly.common.repeatCollectionOnCreated
 import com.synrgy.wefly.data.api.ApiResult
-import com.synrgy.wefly.data.api.login.LoginResponse
 import com.synrgy.wefly.databinding.FragmentHomepageBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -30,32 +31,69 @@ class HomepageFragment : Fragment(R.layout.fragment_homepage) {
         repeatCollectionOnCreated {
             tokenRan()
         }
+        viewModel.getAirportList()
+        observeStateFlow()
     }
 
     private fun setupUI() {
         with(binding) {
-            btnSearchFlight.setOnClickListener { viewModel.getAirportList();observeStateFlow() }
+
         }
     }
+
+
 
     private fun observeStateFlow() {
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.airportList.collect {
-                val content = it.data?.data?.content?.get(1)?.city
+                val content = it.data?.data?.content
                 when(it){
-                    is ApiResult.Loading -> Toast.makeText(context, "Loading", Toast.LENGTH_SHORT).show()
-                    is ApiResult.Success -> Toast.makeText(context, content.toString(), Toast.LENGTH_SHORT).show()
-                    is ApiResult.Error -> Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                    is ApiResult.Loading -> binding.pbHome.visibility = View.VISIBLE
+                    is ApiResult.Success -> {
+                        binding.pbHome.visibility = View.GONE
+                        val city = content?.map { it.city }?.toTypedArray() ?: emptyArray()
+                        departure(city)
+                        arrival(city)
+                        Log.d("neotica", "observeStateFlow: $content")
+                    }
+                    is ApiResult.Error -> {
+                        binding.pbHome.visibility = View.GONE
+                        Log.d("neotica", "observeStateFlow: Error")
+                    }
                 }
             }
         }
     }
 
-    private fun handleSignInResult(status: ApiResult<LoginResponse>) {
-        with(binding) {
+    private fun departure(city: Array<String>){
+        flight(city, binding.spFlightFrom)
+    }
+    private fun arrival(city: Array<String>){
+        flight(city, binding.spFlightTo)
+    }
 
+    private fun flight(arrayDemo: Array<String>, spinner: Spinner){
+        with(binding) {
+            val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, arrayDemo)
+            spinner.adapter = arrayAdapter
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    Log.d("neotica", arrayDemo[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    Log.d("neotica", "Nothing is selected")
+                }
+
+            }
         }
     }
+
     private suspend fun tokenRan () {
         viewModel.token.collect {
             if (it.isEmpty()) {
